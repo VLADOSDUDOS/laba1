ec7d7d07b518ebb102f8ceb886715ff798066208
VLADOSDUDOS <146865995+VLADOSDUDOS@users.noreply.github.com>
2023-10-20 23:06:14 +0300

    Update main.py

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -1,107 +1,87 @@
 import json
 import xml.etree.ElementTree as ET
 
-# Класс для представления жанра кино
-class Genre:
-    def __init__(self, name):
-        self.name = name
-
-# Базовый класс для всех видов медиа (фильмы, сериалы и т. д.)
 class Media:
-    def __init__(self, title, year, director):
+    def __init__(self, title, duration, author):
         self.title = title
-        self.year = year
-        self.director = director
-
-# Класс для представления актеров
-class Actor:
-    def __init__(self, name, birth_year):
-        self.name = name
-        self.birth_year = birth_year
-
-# Класс для представления фильма
-class Movie(Media):
-    def __init__(self, title, year, director, genre, duration, actors):
-        super().__init__(title, year, director)
-        self.genre = genre
         self.duration = duration
-        self.actors = actors
+        self.author = author
 
     def to_dict(self):
         return {
-            "title": self.title,
-            "year": self.year,
-            "director": self.director,
-            "genre": self.genre.name,
-            "duration": self.duration,
-            "actors": [actor.name for actor in self.actors]
+            'title': self.title,
+            'duration': self.duration,
+            'author': self.author
         }
 
-# Класс для представления сериала
+
+class Movie(Media):
+    def __init__(self, title, duration, author, director, genre):
+        super().__init__(title, duration, author)
+        self.director = director
+        self.genre = genre
+
+    def to_dict(self):
+        movie_data = super().to_dict()
+        movie_data.update({
+            'title': self.title,
+            'duration': self.duration,
+            'author': self.author,
+            'director': self.director,
+            'genre': self.genre
+        })
+        return movie_data
+
+
 class TVShow(Media):
-    def __init__(self, title, year, director, seasons, episodes_per_season):
-        super().__init__(title, year, director)
-        self.seasons = seasons
-        self.episodes_per_season = episodes_per_season
-        self.genre = Genre("action")  # Композиция - у сериала есть жанр
+    def __init__(self, title, duration, author, season, episode):
+        super().__init__(title, duration, author)
+        self.season = season
+        self.episode = episode
 
     def to_dict(self):
-        return {
-            "title": self.title,
-            "year": self.year,
-            "director": self.director,
-            "seasons": self.seasons,
-            "episodes_per_season": self.episodes_per_season,
-            "genre": self.genre.name,
-        }
+        tvshow_data = super().to_dict()
+        tvshow_data.update({
+            'title': self.title,
+            'duration': self.duration,
+            'author': self.author,
+            'season': self.season,
+            'episode': self.episode
+        })
+        return tvshow_data
 
-# Создаем собственное исключение для старых фильмов
-class OldMovieException(Exception):
-    def __init__(self, year):
-        self.year = year
-        super().__init__(f"Old Movie Released in {year}")
+# Создаем несколько объектов
+movie1 = Movie("movie 1", 150, "Autor 1", "Director 1", "Comedy")
+movie2 = Movie("Movie 2", 150, "Autor 2", "Director 2", "Comedy")
+tvshow1 = TVShow("TV Show 1", 30, "Autor 1", 1, 3)
+tvshow2 = TVShow("TV Show 2", 30, "Season 1", 2, 3)
 
-try:
-    # Создаем экземпляры классов
-    action_genre = Genre("Action")
-    comedy_genre = Genre("Comedy")
+# Преобразуем объекты в словари
+data = {
+    'movies': [movie1.to_dict(), movie2.to_dict()],
+    'tvshows': [tvshow1.to_dict(), tvshow2.to_dict()]
+}
 
-    actor1 = Actor("Actor 1", 1980)
-    actor2 = Actor("Actor 2", 1990)
+# Сохраняем данные в формате JSON
+with open('data.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
 
-    movie = Movie("Movie 1", 2000, "Director 1", action_genre, 120, [actor1, actor2])
-    tv_show = TVShow("TV Show 1", 2023, "Director 2", 5, 10)
+# Создаем XML-структуру
+root = ET.Element("media")
+for movie in [movie1, movie2]:
+    movie_element = ET.SubElement(root, "movie")
+    for key, value in movie.to_dict().items():
+        ET.SubElement(movie_element, key).text = str(value)
 
-    # Проверяем, если фильм старше 2000 года, вызываем исключение
-    if movie.year < 2000:
-        raise OldMovieException(movie.year)
+for tvshow in [tvshow1, tvshow2]:
+    tvshow_element = ET.SubElement(root, "tvshow")
+    for key, value in tvshow.to_dict().items():
+        ET.SubElement(tvshow_element, key).text = str(value)
 
-    # Сохраняем данные в формате JSON
-    movie_data = movie.to_dict()
-    tv_show_data = tv_show.to_dict()
-
-    with open("movie.json", "w") as movie_file:
-        json.dump(movie_data, movie_file, indent=4)
-
-    with open("tv_show.json", "w") as tv_show_file:
-        json.dump(tv_show_data, tv_show_file, indent=4)
-
-    # Сохраняем данные в формате XML с использованием ElementTree
-    movie_root = ET.Element("Movie")
-    for key, value in movie_data.items():
-        ET.SubElement(movie_root, key).text = str(value)
-
-    tv_show_root = ET.Element("TVShow")
-    for key, value in tv_show_data.items():
-        ET.SubElement(tv_show_root, key).text = str(value)
-
-    movie_tree = ET.ElementTree(movie_root)
-    tv_show_tree = ET.ElementTree(tv_show_root)
-
-    movie_tree.write("movie.xml")
-    tv_show_tree.write("tv_show.xml")
-
-except OldMovieException as e:
-    print(f"Error: {e}")
-except Exception as e:
-    print(f"Error: {e}")
+# Сохраняем данные в формате XML
+tree = ET.ElementTree(root)
+tree.write('data.xml')
+with open('data_from_json.json', 'r') as json_file:
+    loaded_data = json.load(json_file)
+with open('data_to_json.json', 'w') as json_file:
+    json.dump(loaded_data, json_file, indent=4)
